7aa3daa1cd134c92c6e335f307302c8abfc7312d
emilarebrink01 <121689992+emilarebrink01@users.noreply.github.com>
2023-09-07 11:44:30 +0200

    test php
    dag1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+    <?php
+for ($x = 0; $x <= 10; $x++) {
+  echo "The number is: $x <br>";
+}
+?>
+    </body>
+</html>
