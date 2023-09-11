3f1c77d29ed28b1138d226e0f7ea78d0c9c8ac05
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-09-11 12:59:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>PHP Test</title>
-    </head>
-    <body>
+
     <?php
 for ($x = 0; $x <= 10; $x++) {
   echo "The number is: $x <br>";
 }
 ?>
-    </body>
-</html>
