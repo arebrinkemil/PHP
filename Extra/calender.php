8ef788dc7a801c1f235fd9489403403d02338692
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-09-18 21:48:39 +0200

    2.0

--- Extra/calender.php
+++ Extra/calender.php
@@ -1,14 +1,13 @@
 <?php
-// Define the month and year you want information for (e.g., September 2023)
-$month = 7; // 9 represents September
+$month = 1;
 $year = 2023;
 $dateString = "$year-$month-01";
 
 function datum($x)
 {
     $array = [];
-    $firstDayOfWeek = date("l", strtotime($x));
-    $array[] = $firstDayOfWeek;
+    $firstDay = date("l", strtotime($x));
+    $array[] = $firstDay;
     $daysInMonth = date("t", strtotime($x));
     $array[] = $daysInMonth;
     $currentMonth = date("F", strtotime($x));
@@ -18,11 +17,10 @@ function datum($x)
 
 list($firstDay, $monthDays, $monthNow) = datum($dateString);
 
-
+/*
 echo "Month: $monthNow<br>";
 echo "Number of days in the month: $monthDays<br>";
-echo "The month starts with: $firstDay";
-
+echo "The month starts with: $firstDay";*/
 
 ?>
 
@@ -36,7 +34,11 @@ echo "The month starts with: $firstDay";
     <title>Document</title>
     <style>
         div {
-            width: 1500px;
+            width: 90%;
+            margin: 0 auto;
+        }
+
+        ul {
             margin: 0 auto;
         }
 
@@ -44,8 +46,29 @@ echo "The month starts with: $firstDay";
             width: 200px;
             height: 200px;
             border-style: solid;
-            border-radius: 3px;
+            border-radius: 10px;
+            border-width: 4px;
             display: inline-block;
+            margin: 5px;
+            padding: 5px;
+        }
+
+        h1 {
+            font-size: 50px;
+            text-align: center;
+        }
+
+        h2 {
+            font-size: 123px;
+            padding: 0;
+            margin: 0;
+            text-align: center;
+        }
+
+        h3 {
+            font-size: 31px;
+            padding: 10px;
+            margin: 0;
         }
     </style>
 
@@ -53,10 +76,20 @@ echo "The month starts with: $firstDay";
 
 <body>
     <div>
+        <h1><?php echo $monthNow; ?></h1>
+        <h1><?php echo $year; ?></h1>
         <ul>
             <?php
-            for ($i = 0; $i < $monthDays; $i++) {
-                echo "<li></li>";
+            $WeekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            $firstDayIndex = array_search($firstDay, $WeekDay);
+
+            for ($i = 1; $i <= $monthDays; $i++) {
+                $currentDAY = $WeekDay[($firstDayIndex + $i - 1) % 7]; /* ChatGPT ;) */
+                if ($currentDAY == "Saturday" || $currentDAY == "Sunday") {
+                    echo "<li style='background-color: red';><h2>$i</h2><h3>$currentDAY</h3></li>";
+                } else {
+                    echo "<li><h2>$i</h2><h3>$currentDAY</h3></li>";
+                }
             }
             ?>
         </ul>
