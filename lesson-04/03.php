ae9c333d8017286f936e93ea0c6969a41b66ae96
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-09-19 11:03:31 +0200

    KLART

--- lesson-04/03.php
+++ lesson-04/03.php
@@ -1,22 +1,3 @@
-/* <!DOCTYPE html>
-<html>
-<head>
-    <title>Actors List</title>
-</head>
-<body>
-    <ul>
-        <?php foreach ($actors as $actor): ?>
-            <li>
-                <a href="<?php echo $actor['tmdb_url']; ?>"><?php echo $actor['name']; ?></a> as <?php echo $actor['character']; ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html>
-
-*/
-
-
 <?php
 
 $actors = [
@@ -43,6 +24,38 @@ $actors = [
 ];
 
 
-foreach ($actors as $x => $val) {
-    echo "$x = $val<br>";
-}
+echo $actors[2]['name'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <ul>
+
+        <?php
+        foreach ($actors as $actor) { ?>
+            <li>
+
+                <a href="<?php echo $actor['tmdb_url']; ?>"><?php echo $actor['name']; ?></a> -
+                <?php echo $actor['character']; ?>
+
+            </li>
+
+        <?php } ?>
+
+
+    </ul>
+
+
+</body>
+
+</html>
