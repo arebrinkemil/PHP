dc2ff499fff6825112ccd073a5b0767528efec57
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-10-30 11:21:25 +0100

    donedone

--- lesson-15/07.php
+++ lesson-15/07.php
@@ -3,7 +3,24 @@
 // $dir = '/files';
 $files = scandir('files');
 
+function sanitizeName($name)
+{
+    $CleanName = trim(strtolower(filter_var($name, FILTER_SANITIZE_FULL_SPECIAL_CHARS)));
+    return $CleanName;
+}
+
+
+$name = "";
+$content = "";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['name'], $_POST['content'])) {
+        $name = sanitizeName($_POST['name']);
+        $content = sanitizeName($_POST['content']);
+    }
+
+    file_put_contents('files/' . $name . ".txt", $content, FILE_APPEND);
+
     $selectedKey = array_key_first($_POST);
     if (isset($files[$selectedKey])) {
         $text = file_get_contents('files/' . $files[$selectedKey]);
@@ -61,6 +78,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <div class="text">
         <?php echo $text ?>
 
+    </div>
+
+    <div class="container">
+        <div class="row mt-5 justify-content-md-center">
+            <div class="col-md-6 ml-center">
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" name="name" id="name" placeholder="dana-scully.txt" class="form-control" required>
+                        <small class="form-text">Please provide a filename without an extension.</small>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="content" class="form-label">Content</label>
+                        <textarea name="content" id="content" rows="8" cols="80" class="form-control" required></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Add Note</button>
+                </form>
+            </div>
+        </div>
     </div>
 
 </body>
